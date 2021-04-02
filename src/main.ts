86f4f8bf390b97f233109b8fc9a5786b47d43ffc
agdholo <agdholo@gmail.com>
2021-04-02 22:27:37 +0800

    fix(main): import components

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
-import vadmin from "./plugins/vadmin";
+import components from "./plugins/vadmin";
 import "./scss/app.scss";
 
 Vue.config.productionTip = false;
@@ -10,6 +10,6 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   vuetify,
-  vadmin,
+  components,
   render: h => h(App)
 }).$mount("#app");
