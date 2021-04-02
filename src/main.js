56134b3bd088a32308baaa67368224e81dcb71ef
agdholo <agdholo@gmail.com>
2021-04-02 20:37:53 +0800

    feat(app): add class-component support

--- src/main.js
+++ src/main.js
@@ -4,10 +4,7 @@ import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import vadmin from "./plugins/vadmin";
-import VueCompositionAPI from "@vue/composition-api";
 import "./scss/app.scss";
-
-Vue.use(VueCompositionAPI);
 
 Vue.config.productionTip = false;
 
